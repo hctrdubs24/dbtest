336db5379da77074e0ebe18ec17437f1420999be
hctrdubs24 <OtracuentadeHector24@outlook.com>
2022-10-13 19:35:38 -0500

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,43 @@
+$("#add_user").submit(function (event) {
+  alert("¡Usuario creado correctamente!");
+});
+
+$("#update_user").submit(function (event) {
+  event.preventDefault();
+
+  var unindexed_array = $(this).serializeArray();
+  var data = {};
+
+  $.map(unindexed_array, function (n, i) {
+    data[n["name"]] = n["value"];
+  });
+
+  var request = {
+    url: `http://localhost:3000/api/users/${data.id}`,
+    method: "PUT",
+    data: data,
+  };
+
+  $.ajax(request).done(function (response) {
+    alert("¡Usuario actualizado correctamente!");
+  });
+});
+
+if (window.location.pathname == "/") {
+  $ondelete = $(".table tbody td a.delete");
+  $ondelete.click(function () {
+    var id = $(this).attr("data-id");
+
+    var request = {
+      url: `http://localhost:3000/api/users/${id}`,
+      method: "DELETE",
+    };
+
+    if (confirm("¿Está seguro de eliminar este usuario?")) {
+      $.ajax(request).done(function (response) {
+        alert("¡Usuario eliminado correctamente!");
+        location.reload();
+      });
+    }
+  });
+}
